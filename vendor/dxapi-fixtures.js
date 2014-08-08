91879734016268de6bb2f4125966df8928e60018
thriqon <github@jonasw.de>
2014-08-08 14:14:59 +0200

    forgot vendor-file

---
+++ vendor/dxapi-fixtures.js
@@ -0,0 +1,62 @@
+
+/* global require */
+
+require('ic-ajax').defineFixture('http://dx.doi.org/10.1126/science.169.3946.635', {textStatus: 'success', response: {
+  "member": "http://id.crossref.org/member/221",
+  "volume": "169",
+  "indexed": {
+    "timestamp": 1399729759193,
+    "date-parts": [
+      [
+        2014,
+        5,
+        10
+      ]
+    ]
+  },
+  "publisher": "American Association for the Advancement of Science (AAAS)",
+  "source": "CrossRef",
+  "reference-count": 0,
+  "container-title": "Science",
+  "author": [
+    {
+      "given": "H. S.",
+      "family": "Frank"
+    }
+  ],
+  "prefix": "http://id.crossref.org/prefix/10.1126",
+  "score": 1,
+  "issued": {
+    "date-parts": [
+      [
+        1970,
+        8,
+        14
+      ]
+    ]
+  },
+  "subject": [
+    "General"
+  ],
+  "subtitle": [],
+  "page": "635-641",
+  "deposited": {
+    "timestamp": 1309132800000,
+    "date-parts": [
+      [
+        2011,
+        6,
+        27
+      ]
+    ]
+  },
+  "issue": "3946",
+  "title": "The Structure of Ordinary Water: New data and interpretations are yielding new insights into this fascinating substance",
+  "type": "journal-article",
+  "DOI": "10.1126/science.169.3946.635",
+  "ISSN": [
+    "0036-8075",
+    "1095-9203"
+  ],
+  "URL": "http://dx.doi.org/10.1126/science.169.3946.635"
+}});
